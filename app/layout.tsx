73645cd17412729c64aa17b05885b9dc17132518
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2024-01-09 12:13:36 +0330

    clerck

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,32 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
 
-const poppins = Poppins({ subsets: ['latin'],
-weight: ['400', '500','600','700'],
-variable:'--font-poppins'
-})
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
-icons:{
-  icon: '/assets/images/logo.svg'
-}
-}
+  title: "Evently",
+  description: "Evently is a platform for event management.",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
